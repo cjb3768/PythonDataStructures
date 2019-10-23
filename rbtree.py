11f162d5419ac3cc2cc91a7c56e4ccb8f34da405
Cullen Brown <cjb3768@gmail.com>
2019-10-23 12:36:08 -0500

    Implemented Search Function

--- rbtree.py
+++ rbtree.py
@@ -23,3 +23,17 @@ class rbtree_node:
         self.right_child = None
 
     
+    def search(self, root, value):
+
+        #if there is no root, or the root has the value, return root
+        if root == None or root.value == value:
+            logger.debug("Either root value equals value, or there is no root; returning root.")
+            return root
+        #recurse through left tree if value less than root.value
+        elif value < root.value:
+            logger.debug("Value {} less than root value {}; recursing through left subtree".format(value, root.value))
+            return search(root.left_child, value)
+        #otherwise, recurse through right tree (value greater than root.value)
+        else:
+            logger.debug("Value {} greater than root value {}; recursing through right subtree".format(value, root.value))
+            return search(root.right_child, value)
